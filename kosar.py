74d32cc526328defbb2a1a6ac0fc911adfd1cef3
Baaalint <kiss.balint.simon@students.jedlik.eu>
2022-11-10 09:31:33 +0100

    kosarak

--- kosar.py
+++ kosar.py
@@ -1,3 +1,22 @@
+from kosarak import kosarak
+
 print(f'Kosár tartalma:')
 
 f = open('kosar.csv','r', encoding='utf-8')
+
+kosarak = []
+
+res1 = kosarak()
+
+res1.name = 'Mexikói cheddar sajtkrémleves sajtos pizzafedővel'
+res1.type = 'Levesek'
+res1.price = '1390'
+
+kosarak.append(res1)
+
+print(res1)
+print(res1.name)
+print(res1.type)
+print(res1.price)
+
+print(f'{res1.name}-{res1.type}-{res1.price}')

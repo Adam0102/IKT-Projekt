41181d56f2fa5d64d48e0461be83ba5ad2a1d4c3
Károlyi Kevin (9c) <karolyi.kevin@students.jedlik.eu>
2022-11-10 08:40:05 +0100

    probanev

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+
 from adat import *
 from kosar import *
 from rendeles import *
 
 
 name = input('Üdv a KKFood-ban! Kérjük adja meg felhasználónevét: ')
-nameLogin = nameLogingIn(name)
-if nameLogin == False:
+if nameLogingIn(name) == False:
     nameRegister()
-elif nameLogin == True:
+elif nameLogingIn(name) == True:
     menu()
+input('-------------------------------------------\nKérem válasszon az alábbi opciók közül!: \n\tHelybenfogyasztás(elhozom) \n\tElvitel(itthon eszem meg)\n------------------------------------------- \t\n\nA választásom: ')

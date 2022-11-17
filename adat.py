1b36840e802344bd8332353992c56a675c225815
Adam Deli <deli.adam@students.jedlik.eu>
2022-11-17 08:46:05 +0100

    adatok szerkesztése #19 #26

--- adat.py
+++ adat.py
@@ -11,10 +11,19 @@ def logingIn(name,phoneNummber):
     file.close()
 
 def dataRegister(name, dataOfBirth, city, phoneNummber):
-    file = open('adat.csv', 'a', encoding='utf-8')
+    file = open('adat.csv', 'w', encoding='utf-8')
     for row in file:
         splittedData = row.split(';')
-
+        splittedData[0] = int(splittedData[0]) + 1 
+        if splittedData[1] not in file:
+            name.write(file)
+        elif splittedData[2] not in file:
+            dataOfBirth.write(file)
+        elif splittedData[3] not in file:
+            city.write(file)
+        elif splittedData[4] not in file:
+            phoneNummber.write(file)
+    print('Sikeresen felvettük adatait')
     file.close()
 
 def menu():
